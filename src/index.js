5a2d5bcd7ace3bccde66e8df6bbd0fab0dc91259
Jeremy Jones <jeremy@Jeremys-MacBook-Air.local>
2018-04-07 13:57:51 -0700

    updated routes and added new component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Course from './components/Course.js';
+import CourseHome from './components/CourseHome.js';
 import BottomNav from './components/BottomNav.js';
 import TopNav from './components/TopNav.js';
 import Lesson from './components/Lesson.js';
@@ -14,12 +15,10 @@ ReactDOM.render(
       <Route exact path="/" component={Course} />
       <Route exact path="/bcoin/lesson" component={CourseHome} />
       <Route exact path="/lotion/lesson" component={CourseHome} />
+      <Route path="/lotion/lesson/:lsn/chapter/:ch" component={Lesson} />
+      <Route path="/bcoin/lesson/:lsn/chapter/:ch" component={Lesson} />
       <Route path="/lotion/lesson" component={BottomNav} />
       <Route path="/bcoin/lesson" component={BottomNav} />
-      <Route path="/lotion/lesson/:lsn" component={Lesson} />
-      <Route path="/lotion/lesson/:lsn/chapter/:ch" component={Lesson} />
-      <Route path="/bcoin/lesson/:lsn" component={Lesson} />
-      <Route path="/bcoin/lesson/:lsn/chapter/:ch" component={Lesson} />
     </div>
   </Router>,
   document.getElementById('root')
