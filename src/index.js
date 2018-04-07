46911c01a9744495feac0b838e384ec147ee254a
Jeremy Jones <jeremy@Jeremys-MacBook-Air.local>
2018-04-07 13:24:30 -0700

    updated routing

--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<Router>
-		<div>
-			<Route path="/" component={TopNav} />
-			<Route exact path="/" component={Course} />
-			<Route path="/lesson" component={BottomNav} />
-			<Route path="/lotion/lesson/:lsn" component={Lesson} />
-			<Route path="/lotion/lesson/:lsn/chapter/:ch" component={Lesson} />
-			<Route path="/bcoin/lesson/:lsn" component={Lesson} />
-			<Route path="/bcoin/lesson/:lsn/chapter/:ch" component={Lesson} />
-		</div>
-	</Router>,
-	document.getElementById('root')
+  <Router>
+    <div>
+      <Route path="/" component={TopNav} />
+      <Route exact path="/" component={Course} />
+      <Route exact path="/bcoin/lesson" component={CourseHome} />
+      <Route exact path="/lotion/lesson" component={CourseHome} />
+      <Route path="/lotion/lesson" component={BottomNav} />
+      <Route path="/bcoin/lesson" component={BottomNav} />
+      <Route path="/lotion/lesson/:lsn" component={Lesson} />
+      <Route path="/lotion/lesson/:lsn/chapter/:ch" component={Lesson} />
+      <Route path="/bcoin/lesson/:lsn" component={Lesson} />
+      <Route path="/bcoin/lesson/:lsn/chapter/:ch" component={Lesson} />
+    </div>
+  </Router>,
+  document.getElementById('root')
 );
 registerServiceWorker();
