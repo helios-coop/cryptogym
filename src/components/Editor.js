a762af44d8ccb314e489103641a90d1db840f76f
Justin Kaseman <justinkaseman@live.com>
2018-04-07 19:00:02 -0700

    Kinda editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import EditorBox from './EditorBox.js';
 import { EditorContainer } from '../styles.js';
 
-const Editor = props => {
-	return (
-		<EditorContainer>
-			<p>hi</p>
-		</EditorContainer>
-	);
-};
+export default class Editor extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: 1,
+		};
+		this.updateState = this.updateState.bind(this);
+	}
 
-export default Editor;
+	updateState = newText => {
+		console.log(this);
+		console.log(newText);
+		console.log(this.state.text);
+	};
+
+	handleSubmit = newValue => {
+		let newV = this.state.text;
+		newV++;
+		this.setState({ text: newV });
+		console.log(this.state.text);
+	};
+
+	render() {
+		return (
+			<EditorContainer>
+				<EditorBox updateState={this.updateState} />
+				<div onClick={this.handleSubmit}>DIE</div>
+			</EditorContainer>
+		);
+	}
+}
