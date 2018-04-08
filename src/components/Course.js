17474c87eda089a2251466d241dda0858d4c8525
Justin Kaseman <justinkaseman@live.com>
2018-04-07 22:02:10 -0700

    Bedtime changes

--- src/components/Course.js
+++ src/components/Course.js
@@ -9,42 +9,42 @@ const Course = () => {
 			<h1 className="display-4 text-center text-white">Crypto Gym</h1>
 			<HomeContainer>
 				<Container>
-					<div class="card bg-light mb-3">
-						<h3 class="card-header">Build A Coin With Lotion</h3>
-						<div class="card-body">
+					<div className="card bg-light mb-3">
+						<h3 className="card-header">Build A Coin With Lotion</h3>
+						<div className="card-body">
 							<Link to="/lotion/lesson">
-								<h5 class="card-title">
+								<h5 className="card-title">
 									<strong>Lesson 1: </strong>Blockchain
 								</h5>
 							</Link>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 2: </strong>Transaction Handlers
 							</h5>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 3: </strong>Block Handlers
 							</h5>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 4: </strong>Make A Cryptocurrency
 							</h5>
 						</div>
 					</div>
 				</Container>
 				<Container>
-					<div class="card bg-light mb-3">
-						<h3 class="card-header">Build A Wallet With Bcoin</h3>
-						<div class="card-body">
+					<div className="card bg-light mb-3">
+						<h3 className="card-header">Build A Wallet With Bcoin</h3>
+						<div className="card-body">
 							<Link to="/bcoin/lesson">
-								<h5 class="card-title">
+								<h5 className="card-title">
 									<strong>Lesson 1: </strong>Title
 								</h5>
 							</Link>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 2: </strong>Title
 							</h5>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 3: </strong>Title
 							</h5>
-							<h5 class="card-title">
+							<h5 className="card-title">
 								<strong>Lesson 4: </strong>Title
 							</h5>
 						</div>
