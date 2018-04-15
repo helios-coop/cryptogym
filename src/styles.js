d4ee1314eb248e43272622216527a2667e500d05
Justin Kaseman <justinkaseman@live.com>
2018-04-15 14:10:59 -0700

    Logo

--- src/styles.js
+++ src/styles.js
@@ -1,62 +1,60 @@
 import styled from 'styled-components';
 
 export const TNav = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 1%;
-  height: 40px;
-  background-color: #000000;
+	display: flex;
+	justify-content: space-between;
+	margin: 0.7%;
+	background-color: #000000;
 `;
 
 export const Logo = styled.img`
-  font-size: 1.5em;
-  text-align: center;
+	height: 100px;
 `;
 
 export const HomeContainer = styled.div`
-  min-width: 200px;
-  max-width: 1200px;
-  margin: 10%;
+	min-width: 200px;
+	max-width: 1200px;
+	margin: 10%;
 `;
 
 export const ExerciseNav = styled.div`
-  display: flex;
-  background-color: blue;
-  height: 100%;
+	display: flex;
+	background-color: blue;
+	height: 100%;
 `;
 
 export const BNav = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 0 5%;
+	display: flex;
+	justify-content: space-between;
+	margin: 0 5%;
 `;
 
 export const ExerciseContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  color: palevioletred;
-  min-height: 400px;
+	display: flex;
+	justify-content: center;
+	color: palevioletred;
+	min-height: 400px;
 `;
 
 export const ContentContainer = styled.div`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-  width: 100%;
-  min-height: 400px;
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
+	width: 100%;
+	min-height: 400px;
 `;
 
 export const EditorContainer = styled.div`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-  width: 100%;
-  min-height: 400px;
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
+	width: 100%;
+	min-height: 400px;
 `;
 
 export const CourseHomeContainer = styled.div`
-  margin: auto;
-  width: 50%;
-  padding: 10px;
-  color: white;
+	margin: auto;
+	width: 50%;
+	padding: 10px;
+	color: white;
 `;
