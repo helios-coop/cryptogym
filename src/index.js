9f4e6646cb177f76e0fc4668621f174598eb2ca1
Justin Kaseman <justinkaseman@live.com>
2018-04-23 08:48:32 -0700

    merge

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ ReactDOM.render(
   <Router>
     <div>
       <Route path="/" component={TopNav} />
-<<<<<<< HEAD
       <Route exact path="/" component={Home} />
       <Route exact path="/javascript" component={ExercisesList} />
       <Route exact path="/python" component={ExercisesList} />
@@ -22,30 +21,6 @@ ReactDOM.render(
         path="/l/:language/ex/:ex/set/:set/rep/:rep"
         component={Exercise}
       />
-=======
-      <Route exact path="/" component={Course} />
-      <Route exact path="/javascript/exercise" component={CourseHome} />
-      <Route exact path="/python/exercise" component={CourseHome} />
-      <Route exact path="/go/exercise" component={CourseHome} />
-      <Route
-        path="/javascript/exercise/:ex/set/:set/rep/:rep"
-        component={Exercise}
-      />
-      <Route
-        path="/javascript/exercise/:ex/set/:set/rep/:rep"
-        component={BottomNav}
-      />
-      <Route
-        path="/python/exercise/:ex/set/:set/rep/:rep"
-        component={Exercise}
-      />
-      <Route
-        path="/python/exercise/:ex/set/:set/rep/:rep"
-        component={BottomNav}
-      />
-      <Route path="/go/exercise/:ex/set/:set/rep/:rep" component={Exercise} />
-      <Route path="/go/exercise/:ex/set/:set/rep/:rep" component={BottomNav} />
->>>>>>> 54a0df82f6fd1da9559d73368347917a5a80676c
     </div>
   </Router>,
   document.getElementById('root')
