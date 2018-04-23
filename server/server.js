ad7a159e76e9fd3b08e9b2ea7ab29e1b22f62f9a
Justin Kaseman <justinkaseman@live.com>
2018-04-23 00:20:00 -0700

    Jkase/editor (#19)
    * merge
    
    * Smaller images
    
    * Structured data, fixed everything, routes

--- server/server.js
+++ server/server.js
@@ -8,212 +8,30 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-const data = [
-  {
-    id: 1,
-    name: 'Lotion.js',
-    lessons: [
-      {
-        lesson: 1,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText:
-          "In this lesson you will start by learning about blockchain technology and the three protocols that make up a blockchain. Excited? Let's get started!"
-      },
-      {
-        lesson: 2,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      },
-      {
-        lesson: 3,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      },
-      {
-        lesson: 4,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Lotion.js',
-    lessons: [
-      {
-        lesson: 1,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      },
-      {
-        lesson: 2,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      },
-      {
-        lesson: 3,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      },
-      {
-        lesson: 4,
-        chapters: [
-          {
-            chapter: 1,
-            chapterText: ''
-          },
-          {
-            chapter: 2,
-            chapterText: ''
-          },
-          {
-            chapter: 3,
-            chapterText: ''
-          },
-          {
-            chapter: 4,
-            chapterText: ''
-          }
-        ],
-        introText: ''
-      }
-    ]
-  }
-];
+const data = require('./data.js');
 
-server.get('/:module/:lesson', (req, res) => {
-  const { module } = req.params;
-  const { lesson } = req.params;
-  const info = data[module].lessons.filter(
-    lessons => lessons.lesson.toString() === lesson
-  );
-  console.log(info[0].introText);
-  res.status(200).json(info[0].introText);
+server.get('/exercises/:languageNumber', (req, res) => {
+  const lang = req.params.languageNumber;
+
+  if (lang) {
+    const exercises = [];
+    data[lang].exercises.forEach(exercise =>
+      exercises.push(exercise.exerciseName)
+    );
+    res.status(200).json(exercises);
+  } else res.status(422).json('provide language');
 });
 
-// server.get('/:module/:lesson/:chapter', (req, res) => {
-//   const note = notes.filter(note => note.id.toString() === req.params.id)[0];
-//   res.status(200).json(note);
-// });
+server.get('/l/:language/ex/:exercise/set/:set/rep/:rep', (req, res) => {
+  const lang = req.params.language === 'javascript' ? 0 : 1;
+  const { exercise, set, rep } = req.params;
+
+  if (exercise && set && rep) {
+    res
+      .status(200)
+      .json(data[lang].exercises[exercise - 1].sets[set - 1].reps[rep - 1]);
+  } else res.status(422).json('provide inputs');
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log('API is running'));
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BNav } from '../styles.js';
 import { Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 const BottomNav = props => {
-	return (
-		<BNav>
-			<Link to={`${props.match.url}/1/chapter/1`}>
-				<Button color="danger" active>
-					Back
-				</Button>
-			</Link>
-			<Link to={`${props.match.url}/1/chapter/1`}>
-				<Button color="success" active>
-					Next
-				</Button>
-			</Link>
-		</BNav>
-	);
+  const redirectNext = () => {
+    props.next();
+  };
+  const redirectPrev = () => {
+    props.prev();
+  };
+
+  return (
+    <BNav>
+      <Button onClick={redirectPrev} color="danger" active>
+        Back
+      </Button>
+      <Button onClick={redirectNext} color="success" active>
+        Next
+      </Button>
+    </BNav>
+  );
 };
 
 export default BottomNav;
