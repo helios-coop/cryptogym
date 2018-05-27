cd05da1a9792684a6cc6c9d072899e398bff3c98
Justin Kaseman <justinkaseman@live.com>
2018-05-27 14:29:30 -0700

    Jkase/editor (#28)
    * merge
    
    * Smaller images
    
    * Structured data, fixed everything, routes
    
    * merge
    
    * Testing initial setup

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,6 +3,7 @@ import { EditorContainer } from '../styles.js';
 import AceEditor from 'react-ace';
 import { Button } from 'reactstrap';
 import './sum.test.js';
+import './mocha.css';
 
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
@@ -17,72 +18,6 @@ export default class Editor extends Component {
   }
 
   handleSubmit = () => {
-    // danml.com/js/workereval.js
-    function eval2(strCode, cb, blnExecOnly) {
-
-
-      function work() { // this code runs in the worker, providing a safe one-time custom JS enviroment
-        delete Function.prototype.constructor; 	// blocks Function access via any.constructor
-        delete Object.getOwnPropertyNames; 	// prevents environment sniffing
-    
-              // black-list (potentially) unsafe globals to prevent access from user-provided code via formal parameters on a wrapper function:
-        function privacy(self, XMLHttpRequest, importScripts, Function, Worker, WebSocket, MessageChannel, __proto__, __defineGetter__,
-              IDBDatabase, setTimeout, setInterval, EventSource, onmessage, onerror, console) {
-          "use strict"; // makes "eval" keyword even safer by keeping this from execution aliases
-    
-          postMessage(/0/);
-    
-        } /* end privacy() */
-    
-        setTimeout(privacy.bind(null),0); // block 'this' in user-provided code and execute
-    
-      } /* end work() */
-    
-      if (typeof strCode === "function") {
-        strCode = " (" + strCode + ").call()";
-      } else {
-        if(blnExecOnly){
-          strCode="true);"+strCode+";void(0";  
-        }else{ 
-          strCode = "eval(" + JSON.stringify(strCode.trim()) + ")";
-        }
-      }
-    
-      
-    
-      var code = String(work).trim().split("{").slice(1).join("{").slice(0, - 1).trim().replace("/0/", strCode ), // inline the user code
-      worker = new Worker(URL.createObjectURL(new Blob([code]))); // create a new worker loaded with the user-provided code in the wrapper
-    
-      worker.onmessage = function(e) { // code evaluated, results arriving
-        cb(e.data, e, code, worker); // invoke callback with result and some extra arguments for routing 
-        worker.terminate();
-      };
-    
-      worker.onerror = function(e) { // code evaluated, results arriving
-        var m=e.message; 
-        e={toString:function(){return m+"\n"+Object.keys(e.e).map(function(a){
-          if(this[a]==null || typeof this[a]==="object")return;
-          return a+": \t"+this[a]
-                    },e.e).filter(Boolean).join("\n");}, e:e};
-        cb(e, null, code, worker); // invoke callback with result, null as the event object to indicate errror, and some extra arguments for routing 
-        worker.terminate();
-      };
-    
-      return worker;
-
-    }
-
-    //const cb = (input) => console.log(input);
-
-    //eval2(this.refs.aceEditor.editor.session.getValue(), cb)
-    // function sum(a, b) {
-    //   return a + b;
-    // }
-    // describe('sum', function () {
-    //   it('should return sum of arguments', function () {
-    //     chai.expect(sum(1, 2)).to.equal(3);
-    //   });
-    // });
     window.mocha.checkLeaks();
     window.mocha.run();
   };
