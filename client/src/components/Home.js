f742a6ae7ed51898320e6b8f954ef9e946e66683
Justin Kaseman <justinkaseman@live.com>
2018-07-04 23:50:18 -0700

    Justin (#45)
    * Style to home page and exercise list
    
    * fixed untracked files
    
    * gitignore update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,6 @@ import '../styles/Home.css';
 const Home = () => {
   return (
     <div>
-      <div className="d-flex justify-content-center">
         <HomeContainer>
           <h1
             className="display-4 mb-4 mt-2 text-center heading quantico"
@@ -20,12 +19,16 @@ const Home = () => {
           <h3 className="text-center quantico" style={{ color: '#79A7EA' }}>
             Flex Your Blockchain Muscles
           </h3>
-          <p className="lead text-white text-center px-5">
-            CryptoGym is an interactive training center that teaches you
+          <p className="lead text-white text-center">
+            {/* CryptoGym is an interactive training center that teaches you
             blockchain technology one rep at a time. Choose your preferred
-            language and let's get started!
+            language and let's get started! */}
+            You already know blockchain is the next biggest thing since protein powder Adderall shots.<br></br>
+            So why are you are still sitting on your lazy developer ass living a decade in the past?<br></br>
+            {/* Ready to quit your dead-end office job and start a crypto startup where your secretary calls you 'Bitcoin Daddy'? */}
+            It's time to hit some reps and get your brain swole on knowledge!
           </p>
-          <div className="d-flex justify-content-center justify-content-around mt-5">
+          <div className="d-flex justify-content-center justify-content-around mt-3 mb-2">
             <JSLogo>
               <Link to="/javascript" style={{ textDecoration: 'none' }}>
                 <h3 className="text-center text-white my-4 quantico">
@@ -54,9 +57,7 @@ const Home = () => {
               </Link>
             </PythonLogo>
           </div>
-          <br />
         </HomeContainer>
-      </div>
       <Footer />
     </div>
   );
